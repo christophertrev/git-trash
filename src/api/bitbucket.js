25200f6363c1b697f56f712aa15c96463652517f
kureikain <kureikain@gmail.com>
2014-09-25 23:07:01 -0700

    WIP: bitbucket

--- src/api/bitbucket.js
+++ src/api/bitbucket.js
@@ -1,5 +1,33 @@
 var api = {}
 
+api.setParam = function (opt) {
+  this.params = opt
+}
+
+api.loadRepo = function (token, cb) {
+  //Grab github data
+  var url = "https://api.bitbucket.org/2.0/repositories/" + this.params['user']
+  var request = new XMLHttpRequest()
+  request.open('GET', url + "?size=100", true)
+  request.setRequestHeader("Accept", "application/vnd.github.v3+json")
+  request.setRequestHeader("Authorization", "token " + token)
+
+  request.onload = function() {
+    if (request.status >= 200 && request.status < 400){
+      data = JSON.parse(request.responseText);
+      cb(data.values)
+    } else {
+      cb(null)
+    }
+  }
+
+  request.onerror = function() {
+    cb(null)
+  }
+
+  request.send()
+}
+
 api.removeRepo = function (token, repoId, cb) {
   //Grab github data
   var repo = this.state.repo.map(function (repo) {
@@ -10,7 +38,7 @@ api.removeRepo = function (token, repoId, cb) {
   })
   this.setState({repo: repo})
   //return;
-  var url = "https://api.github.com/repos/"
+  var url = "https://api.bitbucket.org/2.0/repositories/" + this.params['user'] + '/' + repoId
   var request = new XMLHttpRequest()
   request.open('DELETE', url + repoId.full_name, true)
   request.setRequestHeader("Accept", "application/vnd.github.v3+json")
@@ -29,3 +57,4 @@ api.removeRepo = function (token, repoId, cb) {
   request.send()
 }
 module.exports = api
+
